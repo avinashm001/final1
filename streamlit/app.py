de9f4d9f2f895879f36cef67b9c37930fddb19fa
avinashm001 <120102387+avinashm001@users.noreply.github.com>
2022-12-08 18:55:11 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,22 +12,19 @@ This the video source of porn collection :[meganz](https://mega.nz/fm/7lFmQRha)'
 
 
 
-def add_bg_from_url():
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-
-
-
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://ftopx.com/275082/0_0.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
-add_bg_from_url() 
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('t.jpg')
 
